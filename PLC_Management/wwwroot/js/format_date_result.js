44c8d75808d34951a50d9e69ebff52070bfb9a68
xuanco941 <xuanco941@gmail.com>
2022-06-24 16:50:52 +0700

    push

--- PLC_Management/wwwroot/js/format_date_result.js
+++ PLC_Management/wwwroot/js/format_date_result.js
@@ -1,11 +1,1 @@
-﻿const today = new Date();
-const yyyy = today.getFullYear();
-let mm = today.getMonth() + 1; // Months start at 0!
-let dd = today.getDate();
-
-if (dd < 10) dd = '0' + dd;
-if (mm < 10) mm = '0' + mm;
-
-today = dd + '/' + mm + '/' + yyyy;
-
-document.getElementById('DATE').value = today;
+﻿
