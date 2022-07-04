63aa5977751b077f1adfffd64ee1d36f80ef06f8
xuanco941 <xuanco941@gmail.com>
2022-07-04 11:58:28 +0700

    add chart

---
+++ PLC_Management/Controllers/ChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PLC_Management.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
